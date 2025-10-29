17d0449cd91afe60560544fb9b5b4629119234d3
Ayesha9848 <kathijashek@gmail.com>
2025-10-29 15:18:34 +0530

    Create tailwind.config.cjs

---
+++ tailwind.config.cjs
@@ -0,0 +1,15 @@
+module.exports = {
+content: ['./index.html','./src/**/*.{js,jsx,ts,tsx}'],
+theme: {
+extend: {
+fontFamily: { inter: ['Inter', 'sans-serif'] },
+colors: {
+heading: '#1E2022',
+subtitle: '#989090',
+logo: '#CF2D2D',
+stroke: '#B0BABF'
+}
+}
+},
+plugins: [],
+}
